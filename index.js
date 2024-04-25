429f0f37ded685429631e64b0e6505d8705f7567
JSG-8579 <rkfntmdrhks@naver.com>
2024-04-25 09:51:54 +0900

    3

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '10mb' }));
 
-const test = require('./router/test.js');
+
 const push = require('./router/push.js');
 const camera = require('./router/camera.js');
 const vue = require('./router/vuetest.js');
 const sudoku = require('./router/sudoku.js');
 
-app.use('/', test);
+
 app.use('/push', push);
 app.use('/camera', camera);
 app.use('/vue', vue);
